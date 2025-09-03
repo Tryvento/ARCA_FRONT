4ae0efae4055610d3abb8cd7c3d79c4b0f6b6ed7
David Carrillo <fixflowcorporativo@gmail.com>
2025-09-03 09:28:17 -0500

    Update suppliers.js

--- src/stores/suppliers.js
+++ src/stores/suppliers.js
@@ -7,17 +7,19 @@ export const useSuppliersStore = defineStore('suppliers', () => {
 
   const getSuppliers = async (nit = null) => {
     try {
+      if (!nit) return []
+      
       const response = await axios.get(`${import.meta.env.VITE_API_URL}/seven_suppliers`, {
-        params: {
-          nit: nit,
-        },
+        params: { nit },
         headers: {
           Authorization: `Bearer ${authStore.userData.token}`,
           'Content-Type': 'application/json',
         },
       })
+      
       console.log('Proveedores obtenidos:', response.data)
-
+      
+      // Return the response data directly, let the component handle the formatting
       return response.data
     } catch (error) {
       console.error('Error al obtener proveedores:', error.response?.data || error.message)
